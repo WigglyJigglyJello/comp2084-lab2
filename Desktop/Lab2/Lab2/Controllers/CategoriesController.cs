c3f66a4d52f76f5716723e5df9d0b641e65e8464
WigglyJigglyJello <ndonaghue9@hotmail.com>
2019-01-28 15:57:04 -0500

    first github push

---
+++ Desktop/Lab2/Lab2/Controllers/CategoriesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab2.Controllers
+{
+    public class CategoriesController : Controller
+    {
+        // GET: Categories
+        public ActionResult Index()
+        {
+            var categories = new List<Category>();
+
+            Category category = new Category();
+            category.Name = "Food";
+            category.Name = "Tech";
+            category.Name = "Sports";
+            categories.Add(Category);
+
+            return View(Category);
+        }
+    }
+}
